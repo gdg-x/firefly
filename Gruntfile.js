8b45125512749dba9b6e81e09c5729be6bd4f001
Michael Prentice <splaktar@gmail.com>
2015-08-09 21:48:08 -0400

    Use grunt serve:dist to start the server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -559,7 +559,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('serve', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'env:all', 'env:prod', 'express:prod', 'wait', 'open', 'express-keepalive']);
+      return grunt.task.run(['build', 'env:all', 'env:prod', 'express:prod', 'wait', 'express-keepalive']);
     }
 
     if (target === 'debug') {
