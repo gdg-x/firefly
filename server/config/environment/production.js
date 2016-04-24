fd89dd84679552a58e723856dfdc303406d32299
Michael Prentice <splaktar@gmail.com>
2016-04-24 18:07:26 -0400

    update(docker): migrate from GAE Managed VMs to Kubernetes (#94)
    * fix(main): map too large on small devices/landscape
    
    media queries to properly fit map to most current phones and tablets
    
    * update(geolocation): update to 0.0.8 version
    
    * update(docker): migrate from GAE Managed VMs to Kubernetes
    
    remove all grunt and dev tooling from container
    only include the dist bundle for node production deployment

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -11,23 +11,19 @@ try {
 
 module.exports = {
   // Server IP
-  ip:       localConfig.SERVER_IP ||
-            process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
+  ip:       process.env.IP ||
+            localConfig.SERVER_IP ||
             undefined,
 
   // Server port
   port:     localConfig.PORT ||
-            process.env.OPENSHIFT_NODEJS_PORT ||
             process.env.PORT ||
-            8080,
+            9000,
 
   // MongoDB connection options
   mongo: {
-    uri:    localConfig.MONGO_URI ||
-            process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME ||
+    uri:    process.env.MONGO_URI ||
+            localConfig.MONGO_URI ||
             'mongodb://localhost/firefly'
   }
 };
